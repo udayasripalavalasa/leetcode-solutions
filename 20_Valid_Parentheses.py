a7bb7d593cfcd8086f8f0c40593fe7a8c9853ef9
Udayasri Palavalasa <udayasripalavalasa486@gmail.com>
2026-07-09 18:11:18 +0530

    Add Valid Parenthese

---
+++ 20_Valid_Parentheses.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def isValid(self, s):
+        stack=[]
+        pairs={
+            ")":"(",
+            "}":"{",
+            "]":"["
+        }
+        for ch in s:
+            if ch in"({[":
+                stack.append(ch)
+            else:
+                if not stack or stack.pop()!=pairs[ch]:
+                    return False
+        return not stack
