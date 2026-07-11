3ab2712104d6b9fb2884c5d859a914c5ec43324f
Udayasri Palavalasa <udayasripalavalasa486@gmail.com>
2026-07-11 22:37:00 +0530

    Add largest odd number in string

---
+++ 1903_Largest_odd_Number_in_string.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def largestOddNumber(self, num):
+        n=len(num)-1
+        for i in range(n,-1,-1):
+            if(int(num[i])%2!=0):
+                return num[:i+1]
+        return ""
+                
+        
